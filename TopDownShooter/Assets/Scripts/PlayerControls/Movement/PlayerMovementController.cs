d69eaeec6e73dfd64b7eff02284dc3bbcd85889b
TalipAlkoc <alkoctalip@gmail.com>
2024-07-04 21:27:23 +0300

    deneme 10

--- TopDownShooter/Assets/Scripts/PlayerControls/Movement/PlayerMovementController.cs
+++ TopDownShooter/Assets/Scripts/PlayerControls/Movement/PlayerMovementController.cs
@@ -10,14 +10,15 @@ namespace TopDownShoother.PlayerControls
     {
         [SerializeField] private InputData _inputData;
         [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private Transform _targetTransform;
         [SerializeField] private PlayerMovementSettings _playerMovementSettings;
 
         private void Update()
         {
             _rigidbody.MovePosition(_rigidbody.position +( _rigidbody.transform.forward * _inputData.Vertical *
                 _playerMovementSettings.SpeedVertical));
-            _rigidbody.MovePosition(_rigidbody.position +( _rigidbody.transform.right * _inputData.Horizontal*
-                _playerMovementSettings.SpeedHorizontal));
+            _targetTransform.Rotate(0, _inputData.Horizontal * _playerMovementSettings.SpeedHorizontal, 0, Space.Self);
+            
         }
 
 
